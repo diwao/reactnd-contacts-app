0c1035d31b7a287010c58d17819566324b2db01b
Daisuke Iwao <nakapom@gmail.com>
2020-12-09 09:43:17 +0900

    Add Reset button

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -17,6 +17,10 @@ class ListContacts extends Component {
     }));
   };
 
+  clearQuery = () => {
+    this.updateQuery('');
+  };
+
   render() {
     const { query } = this.state;
     const { contacts, onDeleteContact } = this.props;
@@ -39,6 +43,16 @@ class ListContacts extends Component {
             onChange={(event) => this.updateQuery(event.target.value)}
           />
         </div>
+
+        {showingContacts.length !== contacts.length && (
+          <div className="showing-contacts">
+            <span>
+              {showingContacts.length} of {contacts.length}
+            </span>
+            <button onClick={this.clearQuery}>Show All</button>
+          </div>
+        )}
+
         <ol className="contact-list">
           {showingContacts.map((contact) => (
             <li key={contact.id} className="contact-list-item">
