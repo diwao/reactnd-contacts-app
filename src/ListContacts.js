54ccc648449cdfb171de5790e9742d556b9cdaee
Daisuke Iwao <nakapom@gmail.com>
2020-12-24 01:34:28 +0900

    Update Create Contact button

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -23,7 +23,7 @@ class ListContacts extends Component {
 
   render() {
     const { query } = this.state;
-    const { contacts, onDeleteContact } = this.props;
+    const { contacts, onDeleteContact, onNavigate } = this.props;
 
     const showingContacts =
       query === ''
@@ -34,7 +34,7 @@ class ListContacts extends Component {
 
     return (
       <div className="list-contacts">
-        <div className="list-contats-top">
+        <div className="list-contacts-top">
           <input
             className="search-contacts"
             type="text"
@@ -42,6 +42,13 @@ class ListContacts extends Component {
             value={query}
             onChange={(event) => this.updateQuery(event.target.value)}
           />
+          <a
+            href="#create"
+            onClick={() => onNavigate()}
+            className="add-contact"
+          >
+            Add Contact
+          </a>
         </div>
 
         {showingContacts.length !== contacts.length && (
